3557ccc7425f3eff98cf949d259db0e8b66836e0
Alex Vo <votanlean@gmail.com>
2022-06-14 18:42:12 +0700

    Try change the message

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ const PUZZLE_NUMBER: u8 = 1;
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
 pub struct Contract {
-    crossword_solution: String,
+    puzzle_solution: String,
 }
 
 #[near_bindgen]
@@ -13,7 +13,7 @@ impl Contract {
     #[init]
     pub fn new(solution: String) -> Self {
         Self {
-            crossword_solution: solution,
+            puzzle_solution: solution,
         }
     }
 
@@ -22,18 +22,18 @@ impl Contract {
     }
 
     pub fn get_solution(&self) -> String {
-        self.crossword_solution.clone()
+        self.puzzle_solution.clone()
     }
 
     pub fn set_solution(&mut self, solution: String) {
-        self.crossword_solution = solution;
+        self.puzzle_solution = solution;
     }
 
     pub fn guess_solution(&mut self, solution: String) -> bool {
         let hashed_input_string = env::sha256(solution.as_bytes());
         let hashed_input_hex = hex::encode(&hashed_input_string);
-        println!("{}, {}", self.crossword_solution, hashed_input_hex);
-        if self.crossword_solution == hashed_input_hex {
+        println!("{}, {}", self.puzzle_solution, hashed_input_hex);
+        if self.puzzle_solution == hashed_input_hex {
             env::log_str("Right!");
             true
         } else {
@@ -70,7 +70,7 @@ mod tests {
     fn debug_get_hash() {
         testing_env!(VMContextBuilder::new().build());
         
-        let debug_solution = "near nomicon ref finance";
+        let debug_solution = "I want to learn near";
         let debug_hash_bytes = env::sha256(debug_solution.as_bytes());
         let debug_hash_string = hex::encode(debug_hash_bytes);
         println!("Let's debug: {:?}", debug_hash_string);
@@ -78,15 +78,15 @@ mod tests {
 
     #[test]
     fn check_guess_solution() {
-        let alice = AccountId::new_unchecked("crossword.votanlean.testnet".to_string());
+        let alice = AccountId::new_unchecked("puzzle.votanlean.testnet".to_string());
         let context = get_context(alice);
         testing_env!(context.build());
 
-        let mut contract = Contract::new("69c2feb084439956193f4c21936025f14a5a5a78979d67ae34762e18a7206a0f".to_string());
+        let mut contract = Contract::new("b78c53475c4892592a74c88b928d77771b115daf6bb41d5350f9cf08cbb5c31a".to_string());
         let mut guess_result = contract.guess_solution("wrong answer here".to_string());
         assert!(!guess_result, "Expected wrong answer");
         assert_eq!(get_logs(), ["Try again."], "Expect fail log");
-        guess_result = contract.guess_solution("near nomicon ref finance".to_string());
+        guess_result = contract.guess_solution("I want to learn near".to_string());
         assert!(guess_result, "Expect right answer!");
         assert_eq!(get_logs(), ["Try again.", "Right!"], "Expect right after fail")
     }
